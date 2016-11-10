b42b7dbe0884c7cb3ecf160eee2d1d7c27317b18
Tulio Coelho <tulioopc@gmail.com>
2016-11-10 11:15:20 -0200

    Scaling test

--- source/Fractal.cpp
+++ source/Fractal.cpp
@@ -8,11 +8,23 @@ int main () {
   int const HEIGHT = 600;
 
   Bitmap bitmap(WIDTH,HEIGHT);
+
+  double min = 999999;
+  double max = -999999;
+
   for (int y = 0; y < HEIGHT; y++) {
-    for (int x =0; x < WIDTH; x++) {
-      bitmap.setPixel(x, y, 255,0,0); // RED
+    for (int x = 0; x < WIDTH; x++) {
+      double x_fractal = (x - WIDTH/2) * 2.0/WIDTH;
+      double y_fractal = (y - HEIGHT/2) * 2.0/HEIGHT;
+
+      if(x_fractal < min) min = x_fractal;
+      if(x_fractal > max) max = x_fractal;
+
+
+      //bitmap.setPixel(x, y, 255,0,0); // RED
     }
   }
+  cout << min << "," << max << endl;
   bitmap.write("test.bmp");
   cout << "Initial" << endl;
   return 0;
