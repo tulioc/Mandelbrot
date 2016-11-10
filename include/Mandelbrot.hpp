447a4552492107b26b74f7179798f84f1cf58024
Tulio Coelho <tulioopc@gmail.com>
2016-11-10 11:34:14 -0200

    Adding Mandelbrot class for algorithm implementation

---
+++ include/Mandelbrot.hpp
@@ -0,0 +1,20 @@
+#ifndef MANDELBROT_H_
+#define MANDELBROT_H_
+
+namespace caveofprogramming {
+
+  class Mandelbrot {
+  public:
+    Mandelbrot();
+    virtual ~Mandelbrot();
+
+    static int getIterations(double x, double y);
+    static const int MAX_ITERATIONS = 1000;
+
+};
+
+
+
+} /*namespace caveofprogramming*/
+
+#endif
---
+++ source/Mandelbrot.cpp
@@ -0,0 +1,19 @@
+#include "../include/Mandelbrot.hpp"
+
+namespace caveofprogramming {
+
+
+  Mandelbrot::Mandelbrot() {
+
+  }
+
+  Mandelbrot::~Mandelbrot() {
+
+  }
+
+  int Mandelbrot::getIterations(double x, double y) {
+    return 0;
+  }
+
+
+} /*namespace caveofprogramming*/
