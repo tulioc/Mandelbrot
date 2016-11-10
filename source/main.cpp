baf6deb04bb36676503afe99794e675a17c9ac6d
Tulio Coelho <tulioopc@gmail.com>
2016-11-10 12:24:29 -0200

    Improving the image

--- source/main.cpp
+++ source/main.cpp
@@ -16,15 +16,18 @@ int main () {
 
   for (int y = 0; y < HEIGHT; y++) {
     for (int x = 0; x < WIDTH; x++) {
-      double x_fractal = (x - WIDTH/2) * 2.0/WIDTH;
+      double x_fractal = (x - WIDTH/2 - 200) * 2.0/HEIGHT;
       double y_fractal = (y - HEIGHT/2) * 2.0/HEIGHT;
 
       int iterations = Mandelbrot::getIterations(x_fractal, y_fractal);
 
-      uint8_t red = (uint8_t)(256*(double)iterations/Mandelbrot::MAX_ITERATIONS);
-      bitmap.setPixel(x, y, red,red,red); // RED
-      if(red < min) min = red;
-      if(red > max) max = red;
+      uint8_t color = (uint8_t)(256*(double)iterations/Mandelbrot::MAX_ITERATIONS);
+
+      color = color*color*color;
+
+      bitmap.setPixel(x, y, 0,color,0); // RED
+      if(color < min) min = color;
+      if(color > max) max = color;
     }
   }
   cout << min << "," << max << endl;
